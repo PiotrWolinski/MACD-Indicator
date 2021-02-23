32d2b0089d4bc256ab1f65b5ced849c17b2d38c3
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2021-02-24 00:18:35 +0100

    Done for today

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import pandas as pd
+from pandas import DataFrame
 
 URL = 'https://stooq.pl/q/d/l/?s=wig20&i=d'
+SET_SIZE = 1000
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv(URL)
 
-N = len(df.index)
+def EMA(df: DataFrame, period: int, offset: int) -> float:
+    a = 2 / (period + 1)
+    cur_day = len(df.index) - offset
+    upper_sum = 0.0
+    lower_sum = 0.0
 
-print(df.to_string())
-print(N)
+    power = 0
+    for i in range(cur_day - 1, cur_day - 1 - period - 1, -1):
+        upper_sum += df.loc[i][4] * (1 - a) ** power
+        lower_sum += (1 - a) ** power
+        power += 1
+
+    return upper_sum / lower_sum
+
+def get_MACD(df: DataFrame) -> list[float]:
+    MACD = []
+
+    for i in range(SET_SIZE):
+        MACD.append(EMA(df, period=12, offset=i) - EMA(df, period=26, offset=i))
+
+    return MACD
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
